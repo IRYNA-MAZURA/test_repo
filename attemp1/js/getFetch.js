5c0af4f88e565e38691cc5728305a55d1ddd86f8
kharuk <frends.nastya@gmail.com>
2018-11-23 22:21:01 +0300

    update

--- attemp1/js/getFetch.js
+++ attemp1/js/getFetch.js
@@ -133,37 +133,37 @@ let videoCount;
 window.onresize = function () {
     console.log('onResize');
     console.log('qwerty'+videoCount);
-    if (window.innerWidth <= 1170 && videoCount == 4) {
+    if (window.screen.width <= 1170 && videoCount == 4) {
         document.getElementsByClassName('container')[3].remove();
         videoCount = 3;
         console.log('ss1'+videoCount);
     }
 
-    if (window.innerWidth <= 860 && videoCount == 3) {
+    if (window.screen.width <= 860 && videoCount == 3) {
         document.getElementsByClassName('container')[2].remove();
         videoCount = 2;
         console.log('ss2'+videoCount);
     }
 
-    if (window.innerWidth <= 595 && videoCount == 2) {
+    if (window.screen.width <= 595 && videoCount == 2) {
         document.getElementsByClassName('container')[1].remove();
         videoCount = 1;
         console.log('ss3'+videoCount);
     }
 
 
-    if (window.innerWidth > 595 && videoCount == 1) {
+    if (window.screen.width > 595 && videoCount == 1) {
         DrawInfo(arr[indexOfLeftVideo+1]);
         videoCount = 2;
         console.log(videoCount);
     }
 
-    if (window.innerWidth > 860 && videoCount == 2) {
+    if (window.screen.width > 860 && videoCount == 2) {
         DrawInfo(arr[indexOfLeftVideo+2]);
         videoCount = 3;
     }
 
-    if (window.innerWidth > 1170 && videoCount == 3) {
+    if (window.screen.width > 1170 && videoCount == 3) {
         DrawInfo(arr[indexOfLeftVideo+4]);
         videoCount = 4;
     }
@@ -172,12 +172,12 @@ window.onresize = function () {
 
 function getCurrentVideoCount(){
     console.log(innerWidth);
-    if(window.innerWidth <= 595)
+    if(window.screen.width <= 595)
         return 1;
     else
-        if(window.innerWidth >= 595)
-            if(window.innerWidth >= 861)
-                if(window.innerWidth >= 1170)
+        if(window.screen.width >= 595)
+            if(window.screen.width >= 861)
+                if(window.screen.width >= 1170)
                     return 4;
                 else
                     return 3;
